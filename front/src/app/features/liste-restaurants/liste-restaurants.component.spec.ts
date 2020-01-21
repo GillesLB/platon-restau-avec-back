859da3c0931c15ee9326a0a0f90f95055eab5ff5
GillesLB <deltag@laposte.net>
2020-01-21 17:07:20 +0100

    Commit tests 01 ok

--- front/src/app/features/liste-restaurants/liste-restaurants.component.spec.ts
+++ front/src/app/features/liste-restaurants/liste-restaurants.component.spec.ts
@@ -6,6 +6,7 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {MomentModule} from 'ngx-moment';
 import {HttpClientModule} from '@angular/common/http';
 import { By } from '@angular/platform-browser';
+import { of } from 'rxjs';
 
 describe('ListeRestaurantsComponent', () => {
   let component: ListeRestaurantsComponent;
@@ -31,6 +32,7 @@ describe('ListeRestaurantsComponent', () => {
     component = fixture.componentInstance;
     component.listeRestaurants = {
       date_visite: '2020-01-20',
+      nb_visite: 3,
     };
     fixture.detectChanges();
   });
@@ -39,10 +41,24 @@ describe('ListeRestaurantsComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should show the good date', () => {
-    const newDate = fixture.debugElement.query(By.css('.text-center'));
-    console.log('t : ', newDate);
-    fixture.detectChanges();
-    expect(component.listeRestaurants.date_visite.trim()).toBe('20-01-2020');
+  it('should contain date', () => {
+    expect(component.listeRestaurants.date_visite).toContain('2020-01-20');
   });
+
+  it('should be greater than 2', () => {
+    expect(component.listeRestaurants.nb_visite).toBeGreaterThan(2);
+  });
+
+  it('should focus on a restaurant', () => {
+    const restaurant = {nom: 'Truc'};
+    component.onFocus(restaurant);
+    spyOn(component, 'onFocus').and.returnValue(of(restaurant.nom))
+    expect(component.selectedRestaurant['nom']).toEqual('Truc');
+  });
+
+  // it('should show the good date', () => {
+  //   fixture.detectChanges();
+  //   const subscriptionName = fixture.nativeElement.querySelector('.bla').textContent;
+  //   expect(subscriptionName).toContain('20-01-2020');
+  // });
 });
